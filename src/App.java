7753666b188b6e289bc231031307bb1c698f3982
veriditious8 <248295711+veriditious8@users.noreply.github.com>
2026-08-16 20:06:13 +0300

    gave random examples check it outtt

--- src/App.java
+++ src/App.java
@@ -10,6 +10,7 @@ public class App {
         boolean waow = true;
         while (waow){
             int choice = sc.nextInt();
+            giveRandomExample();
             System.out.println("Welcome to the Request Management System.");
             System.out.println("1. Add a new service request");
             System.out.println("2. Find a service request by ID");
@@ -107,8 +108,48 @@ public class App {
                     System.out.println("Invalid choice. Please try again.");
             }
 
-        }
-    }
 
+
+
+        }
+
+
+
+
+    }
+//the 20 samples
+    
+    private static void giveRandomExample(){
+
+        /*
+        category, location, status, year, month, day, urgency - i keep forgetting the order vro
+        */
+            unifiedDS dsEX = new unifiedDS();
+            dsEX.insertRequest(0, 0, 0, "2025", "01", "14", 3);
+            dsEX.insertRequest(1, 2, 1, "2025", "11", "26", 1);
+            dsEX.insertRequest(4, 3, 3, "2026", "03", "11", 5);
+            dsEX.insertRequest(0, 0, 2, "2026", "06", "19", 3);
+            dsEX.insertRequest(5, 1, 1, "2025", "02", "20", 4);
+            dsEX.insertRequest(2, 2, 2, "2024", "10", "13", 5);
+            dsEX.insertRequest(4, 0, 3, "2024", "03", "15", 1);
+            dsEX.insertRequest(1, 1, 1, "2026", "02", "02", 2);
+            dsEX.insertRequest(2, 2, 2, "2025", "03", "10", 5);
+            dsEX.insertRequest(3, 0, 0, "2024", "01", "15", 4);
+            dsEX.insertRequest(1, 1, 1, "2024", "02", "20", 2);
+            dsEX.insertRequest(4, 2, 2, "2025", "12", "10", 1);
+            dsEX.insertRequest(3, 1, 3, "2024", "04", "28", 3);
+            dsEX.insertRequest(1, 3, 1, "2026", "02", "20", 2);
+            dsEX.insertRequest(2, 1, 3, "2025", "03", "10", 1);
+            dsEX.insertRequest(5, 0, 0, "2025", "01", "15", 4);
+            dsEX.insertRequest(1, 1, 1, "2026", "02", "06", 2);
+            dsEX.insertRequest(2, 2, 2, "2026", "11", "23", 1);
+            dsEX.insertRequest(5, 3, 0, "2026", "01", "15", 3);
+            dsEX.insertRequest(4, 2, 3, "2026", "09", "09", 1);
+            
+
+        }
+
+
+    
 
 }

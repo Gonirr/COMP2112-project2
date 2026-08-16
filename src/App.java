4e788959c26106e479b8734ac06f9922f7ea0b91
veriditious8 <248295711+veriditious8@users.noreply.github.com>
2026-08-16 16:41:33 +0300

    man I'm so hungry I could eat a baget...

--- src/App.java
+++ src/App.java
@@ -1,5 +1,65 @@
+import java.util.Scanner;
+//I tried importing request like twice it wouldnt let me can you do it if its needed??
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("a");
+
+    private unifiedDS ds = new unifiedDS();
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean waow = true;
+
+        while (waow){
+            int choice = sc.nextInt();
+            System.out.println("Welcome to the Request Management System.");
+            System.out.println("1. Add a new service request");
+            System.out.println("2. Find a service request by ID");
+            System.out.println("3. List all requests by priority order");
+            System.out.println("4. See the most urgent active request");
+            System.out.println("5. Update request status");
+            System.out.println("6. Update request urgency");
+            System.out.println("7. Delete a request");
+            System.out.println("8. Show requests by location");
+            System.out.println("9. Show data structure statistics");
+            System.out.println("0. Exit\n");
+
+            switch (choice) {//these are placeholder methods so I don't forget the names I'll brb
+                case 1:
+                    ds.addRequest();
+                    break;
+                case 2:
+                    ds.findRequest();
+                    break;
+                case 3:
+                    ds.listRequestsByPriority();
+                    break;
+                case 4:
+                    ds.showMostUrgentRequest();
+                    break;
+                case 5:
+                    ds.updateRequestStatus();
+                    break;
+                case 6:
+                    ds.updateRequestUrgency();
+                    break;
+                case 7:
+                    ds.deleteRequest();
+                    break;
+                case 8:
+                    ds.showRequestsByLocation();
+                    break;
+                case 9:
+                    ds.showStatistics();
+                    break;
+                case 0:
+                    waow = false;
+                    System.out.println("Exiting the program. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+
+        }
     }
+
+
 }
